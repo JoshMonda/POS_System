81d282885e532a05e85edbd6abaeb60cacbaf3d7
Joash Monda <56483606+JoshMonda@users.noreply.github.com>
2022-08-21 10:55:01 +0300

    Initial Commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 4.0.3
+    </div>
+    <strong>Copyright &copy; 2021-2022  <a href="https://juventusworld.online">Juventus World Digital Tech Solution</a>.</strong> All rights
+    reserved developed by Joash Monda
+  </footer>
